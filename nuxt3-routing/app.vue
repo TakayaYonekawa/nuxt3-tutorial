f2b4bf5c3d35c78c66e4ceaf13fef7d98dce4a10
米川貴也 <zuomujianwu1@gmail.com>
2023-07-26 21:47:52 +0900

    画像を表示

--- nuxt3-routing/app.vue
+++ nuxt3-routing/app.vue
@@ -1,18 +1,12 @@
 <template>
   <div>
-    <ul>
-      <li v-for="user in users" :key="user.id">
-        {{user.id}}, {{user.name}}
-      </li>
-    </ul>
+    <img src="~/assets/cat.jpg" alt="">
   </div>
 
 </template>
 
-<script setup>
-  const {data: users, error} = await useFetch('https://jsonplaceholder.typicode.com/userssss/')
-  // console.log(error.value);
-  if(error.value){
-    throw createError({statusCode: 404, statusMessage: 'Page not found!'})
-  }
-</script>
+<style>
+img{
+  width: 100%;
+}
+</style>
