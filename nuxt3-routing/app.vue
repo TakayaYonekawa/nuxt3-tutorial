ca863d5b86d29ee5546f219ec3b55f953022a6b8
米川貴也 <zuomujianwu1@gmail.com>
2023-07-26 17:33:52 +0900

    font読み込み

--- nuxt3-routing/app.vue
+++ nuxt3-routing/app.vue
@@ -5,3 +5,14 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+  useHead(
+    {
+      title: "Nuxt 3 store",
+      link:[
+        {rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Permanent+Marker&family=Poppins:ital,wght@0,500;0,600;0,700;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,100;1,300&display=swap"}
+      ]
+    }
+  )
+</script>
--- nuxt3-routing/layouts/default.vue
+++ nuxt3-routing/layouts/default.vue
@@ -1,17 +1,20 @@
 <template>
-    <div class="container">
-        <slot />
-        <footer>copy right</footer>
-    </div>
+  <div class="container">
+    <slot />
+    <footer>copy right</footer>
+  </div>
 </template>
 
 <style>
-    .container{
-        color: red;
-    }
-    footer{
-        background-color: orange;
-        color: white;
-        text-align: center;
-    }
+html {
+  font-family: "Permanent Marker", cursive;
+}
+.container {
+  color: red;
+}
+footer {
+  background-color: orange;
+  color: white;
+  text-align: center;
+}
 </style>
