147b442777f3b9200f8967bb38f33c1a9ea590cd
米川貴也 <zuomujianwu1@gmail.com>
2023-08-10 17:13:26 +0900

    初期化二回目

--- nuxt3-routing/app.vue
+++ nuxt3-routing/app.vue
@@ -1,5 +1,17 @@
 <template>
-  
+      <div>
+      <form >
+        <input  autocomplete="off" type="text"/>
+        <button>Add</button>
+      </form>
+      <ul>
+        <li  key="task">
+          <span>{{}}</span>
+          <button >Delete</button>
+        </li>
+      </ul>
+      <button >Clear</button>
+    </div>
 </template>
 
 <script>
