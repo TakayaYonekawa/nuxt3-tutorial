9c701c63275acb6a61df66bd582af39bb57a7c6b
米川貴也 <zuomujianwu1@gmail.com>
2023-08-13 14:36:41 +0900

    データオプション

--- vue-practice/main.js
+++ vue-practice/main.js
@@ -1,3 +1,14 @@
 const app = Vue.createApp({
+    data: () => ({
+        message: 'Hello Vue.js!',
+        count:99,
+        user: {
+            lastName: 'Nakamura',
+            firstName: 'Yuta',
+            prefecture: 'Tokyo'
+        },
+        colors: ['Red', 'Green', 'Blue']
+    })
+
 
 }).mount('#app')
