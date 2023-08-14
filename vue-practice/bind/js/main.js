bb1039cbd6267d40bfa44c43544bf9a2e74015af
米川貴也 <zuomujianwu1@gmail.com>
2023-08-14 11:57:20 +0900

    単位変換

--- vue-practice/bind/js/main.js
+++ vue-practice/bind/js/main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data: () => ({
-        message: 'hello'
+        km: 0,
+        m: 0
     }),
     computed: {
 
@@ -9,9 +10,16 @@ const app = Vue.createApp({
 
     },
     watch:{
-        message: function(newValue, oldValue){
-            console.log('new: %s, old: %s', newValue, oldValue);
+        km: function(value){
+            console.log(value);
+            this.km = value
+            this.m = value * 1000
+        },
+        m: function(value){
+            this.km = value / 1000
+            this.m = value
         }
+
     }
 })
 
