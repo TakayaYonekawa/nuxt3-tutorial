e3e1cdbd15bba0b61fe302c5c14adf508afc959d
米川貴也 <zuomujianwu1@gmail.com>
2023-08-15 11:38:50 +0900

    clickHandler

--- vue-practice/bind/js/main.js
+++ vue-practice/bind/js/main.js
@@ -4,6 +4,11 @@ const app = Vue.createApp({
     data: () => ({
         counter: 0
     }),
+    methods: {
+        clickHandler: function(){
+            this.counter++
+        }
+    },
     watch:{
 
     },
