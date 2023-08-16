6c8a079c05d58a3b51e96d8f1d833a4d79b30815
米川貴也 <zuomujianwu1@gmail.com>
2023-08-16 18:04:11 +0900

    API練習

--- vue-practice/bind/js/main.js
+++ vue-practice/bind/js/main.js
@@ -1,22 +1,19 @@
 const app = Vue.createApp({
     data: () => ({
-        newItem: '',
-        todos: []
-    }),
-    methods: {
-        addTodo: function(e){
-            if(this.newItem === '') return
-            let todo = {
-                item : this.newItem,
-                isDone: false
-            }
-            this.todos.push(todo)
-            this.newItem = ''
-        },
-        deleteTodo: function(index){
-            this.todos.splice(index, 1)
-        }
 
-    }
-  })
-  app.mount('#app')
+        
+    }),
+    watch:{
+
+    },
+    mounted: function(){
+
+    },
+    methods: {
+
+    } 
+        
+})
+
+
+app.mount('#app')
