3ac327561e609dd0f876df698ba36df6c6970f6b
米川貴也 <zuomujianwu1@gmail.com>
2023-08-16 17:26:16 +0900

    todo完成

--- vue-practice/bind/js/main.js
+++ vue-practice/bind/js/main.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
     data: () => ({
-
+        newItem: '',
+        todos: []
     }),
     methods: {
-
+        addTodo: function(e){
+            if(this.newItem === '') return
+            let todo = {
+                item : this.newItem,
+                isDone: false
+            }
+            this.todos.push(todo)
+            this.newItem = ''
+        },
+        deleteTodo: function(index){
+            this.todos.splice(index, 1)
+        }
 
     }
   })
