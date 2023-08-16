cad00a77be9f979a72070c538d64fe307253ccab
米川貴也 <zuomujianwu1@gmail.com>
2023-08-16 10:55:49 +0900

    transition

--- vue-practice/bind/js/main.js
+++ vue-practice/bind/js/main.js
@@ -1,21 +1,10 @@
-const buttonCounter = {
-    template: '<div><span>count: </span><button @click="countUp">{{ count }}</button></div>',
-    data: () => ({
-        count: 0
-    }),
-    methods: {
-        countUp: function(event){
-            this.count++
-        }
-    }
-}
+
 
 const app = Vue.createApp({
     data: () => ({
-        
+        show: true
     }),
     components: {
-        'button-counter': buttonCounter
     },
     methods: {
 
