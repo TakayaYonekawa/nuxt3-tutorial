4a9d212049c5e2ee21529eb441d16c54cfcf0623
米川貴也 <zuomujianwu1@gmail.com>
2023-08-16 16:49:03 +0900

    todo練習

--- vue-practice/bind/js/main.js
+++ vue-practice/bind/js/main.js
@@ -1,23 +1,10 @@
-
-
 const app = Vue.createApp({
     data: () => ({
-        show: true
+
     }),
-    components: {
-    },
     methods: {
 
-    },
-    watch:{
 
-    },
-
-
-        
-})
-
-app.component('hello-component', {
-    template: '<p>Hello</p>'
-})
-app.mount('#app')
+    }
+  })
+  app.mount('#app')
