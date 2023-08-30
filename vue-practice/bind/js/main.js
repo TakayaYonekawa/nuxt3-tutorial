c838d537dd54cd51c2878da3c027599b10af46c3
米川貴也 <zuomujianwu1@gmail.com>
2023-08-30 20:02:30 +0900

    todo練習完了

--- vue-practice/bind/js/main.js
+++ vue-practice/bind/js/main.js
@@ -1,15 +1,32 @@
 const app = Vue.createApp({
     data: () => ({
-
-        
+        newTodo: '',
+        todos: []
     }),
     watch:{
 
     },
     mounted: function(){
-
+        this.todos = JSON.parse(localStorage.getItem('todos'))
+        if(!this.todos){
+            this.todos = []
+        }
     },
     methods: {
+        addTodo: function(){
+            if(this.newTodo === '') return
+            let todo = {
+                item: this.newTodo,
+                isDone: false
+            }
+            this.todos.push(todo)
+            localStorage.setItem('todos', JSON.stringify(this.todos))
+            this.newTodo = ''
+        },
+        deleteTodo: function(index){
+            this.todos.splice(index, 1)
+            localStorage.setItem('todos', JSON.stringify(this.todos))
+        }
 
     } 
         
