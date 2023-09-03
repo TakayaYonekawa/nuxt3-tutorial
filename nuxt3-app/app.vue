351fbb0cea7d564921af9fed739d7b087bb7a97d
米川貴也 <zuomujianwu1@gmail.com>
2023-09-03 18:22:08 +0900

    Todo練習　nuxt3-app

--- nuxt3-app/app.vue
+++ nuxt3-app/app.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <NuxtWelcome />
+   <div>
+    <h1>To Do</h1>
+    <form >
+      <input type="text" >
+      <button>Add </button>
+    </form>
+
+    <ul>
+      <li >
+        <span></span>
+        <button >Delete</button>
+      </li>
+    </ul>
+
+    <button >Clear</button>
   </div>
+
 </template>
+
+<script setup>
+
+</script>
--- nuxt3-app02/app.vue
+++ nuxt3-app02/app.vue
@@ -1,13 +1,44 @@
 <template>
   <div>
-    <p></p>
-    <button >+</button>
-    <button >-</button>
-    <button >Reset</button>
+    <h1>To Do</h1>
+    <form @submit.prevent="addTask">
+      <input type="text" v-model="newTask" name="newTask" autocomplete="off">
+      <button>Add </button>
+    </form>
+
+    <ul>
+      <li v-for="(task, index) in tasks" :key="task">
+        <span>{{task}}</span>
+        <button @click="deleteTask(index)">Delete</button>
+      </li>
+    </ul>
+
+    <button @click="ClearTask">Clear</button>
   </div>
 </template>
 
 
 <script setup>
+  const tasks = useCookie(
+    'tasks',
+    {
+      default: () => []
+    }
+  )
 
+  const newTask = ref('')
+  function addTask(){
+    if(newTask.value.length >= 1){
+      tasks.value.push(newTask.value)
+    }
+    newTask.value = ''
+  }
+
+  function deleteTask(index){
+    tasks.value.splice(index, 1)
+  }
+
+  function ClearTask(){
+    tasks.value = []
+  }
 </script>
