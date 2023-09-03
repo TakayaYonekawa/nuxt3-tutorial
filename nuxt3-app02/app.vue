8e40f23d1d6fcd565f091bf11a77c6ad611c978e
米川貴也 <zuomujianwu1@gmail.com>
2023-09-03 15:47:56 +0900

    counter練習

--- nuxt3-app02/app.vue
+++ nuxt3-app02/app.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    <p>{{counter}}</p>
-    <button @click="counterStore.countUp">+</button>
-    <button @click="counterStore.countDown">-</button>
-    <button @click="counterStore.Reset">Reset</button>
+    <p></p>
+    <button >+</button>
+    <button >-</button>
+    <button >Reset</button>
   </div>
 </template>
 
 
 <script setup>
-  const counterStore = useCounterState()
-  const {counter} = counterStore
+
 </script>
