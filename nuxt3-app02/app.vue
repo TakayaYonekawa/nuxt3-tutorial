c4d54e9f13cea854588e4d9cee33ceb8054934c5
米川貴也 <zuomujianwu1@gmail.com>
2023-09-03 15:47:03 +0900

    完成

--- nuxt3-app02/app.vue
+++ nuxt3-app02/app.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <p>{{counter}}</p>
+    <button @click="counterStore.countUp">+</button>
+    <button @click="counterStore.countDown">-</button>
+    <button @click="counterStore.Reset">Reset</button>
   </div>
 </template>
+
+
+<script setup>
+  const counterStore = useCounterState()
+  const {counter} = counterStore
+</script>
